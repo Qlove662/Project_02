fc4a0c5e7a97c56cffedf148769dbe6341e10079
Qlove662 <love.jones84@yahoo.com>
2024-10-28 19:19:46 -0500

    created API and pages created

---
+++ script.js
@@ -0,0 +1,12 @@
+async function displayData() {
+    const giphyApi = "PjvdqKiTPO2mLJwv0Z5YBjdHulb8coGn";
+    const indPoinGet = 
+    https://giphy.com/search/robot
+
+    try {
+        const response = await fetch(indPoinGet);
+        console.log (response);
+        let data 
+        });
+    
+}
