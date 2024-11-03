30c7057d210b11e0d71deb9a78953f8cf6d088b4
Qlove662 <love.jones84@yahoo.com>
2024-11-03 17:47:48 -0600

    project 02 page added

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Project_02
+Project_02:  Gif/Website Search Engine
+
+
+Project 02 builds on HTML, CSS, and JavaScript. I am creating a GIFs search engine using an API. This project will require a search bar to
+search for Gifs.
+
+
+Technology:
+
+.HTML
+.CSS
+.JS
+
+
+Ideas:
+
+Interactive navbar that has a Gif image.
+
+Menu with options and images that are clickable.
+
+Search results with an enter button and ability to click the search button.
+
+
+User Stories:
+
+As a user, I need to to copy or paste Gifs that can be shared.
+
+As a user, I need to search Gifs by entering a keyword or phrase.
+
+As a user, I need Gif options when I enter keywords or phrases.
+
+As a user, I need to see and view all of the Gif options in rows or columns.
+
+As a user, I want the images to easily viewed and organized.
+
+
+Wireframe:
--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-async function displayData() {
+async function displayData(searchTerm) {
     const giphyApi = "PjvdqKiTPO2mLJwv0Z5YBjdHulb8coGn";
-    const indPoinGet = 
-    https://giphy.com/search/robot
-
-    try {
-        const response = await fetch(indPoinGet);
-        console.log (response);
-        let data 
-        });
+    const endPintGet = 'https://api.giphy.com/v1/gifs/search?api_key=PjvdqKiTPO2mLJwv0Z5YBjdHulb8coGn&q=robot&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips';
+    const endPointGet2 = 'https://api.giphy.com/v1/gifs/search?api_key=${giphyApi}&q=robot&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips';
     
-}
+    try {
+        const response = await fetch (endPointGet);
+        if (!response.ok) {
+          throw new Error("Response was not successful");
+        }
+        let data = await response.json();
+        let section_image = document.getElementById("section_image");
+        section_image.innerHTML = "";
+    
+        data.data.forEach((element) => {
+          const newImage = document.createElement("img");
+          newImage.src = element.images.original.url;
+          newImage.className = "giphy-img";
+          section_image.appendChild(newImage);
+        });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    
+    document.getElementById("searchButton").addEventListener("click", () => {
+      const searchTerm = document.getElementById("searchTerm").value;
+      if (searchTerm) {
+        displayData(searchTerm);
+      } else {
+        alert("Please enter a search term");
+      }
+    });
