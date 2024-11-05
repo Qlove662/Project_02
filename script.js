cca70829988766e5a196c87368c1f7f3ed0ae478
Qlove662 <love.jones84@yahoo.com>
2024-11-04 18:20:55 -0600

    corrected error endPointGet to endPintGet

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 async function displayData(searchTerm) {
     const giphyApi = "PjvdqKiTPO2mLJwv0Z5YBjdHulb8coGn";
     const endPintGet = 'https://api.giphy.com/v1/gifs/search?api_key=PjvdqKiTPO2mLJwv0Z5YBjdHulb8coGn&q=robot&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips';
-    const endPointGet2 = 'https://api.giphy.com/v1/gifs/search?api_key=${giphyApi}&q=robot&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips';
+    const endPintGet2 = 'https://api.giphy.com/v1/gifs/search?api_key=${giphyApi}&q=robot&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips';
     
     try {
-        const response = await fetch (endPointGet);
+        const response = await fetch (endPintGet);
         if (!response.ok) {
           throw new Error("Response was not successful");
         }
